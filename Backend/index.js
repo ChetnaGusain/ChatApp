7f11821dc1750e79d7e9366a9ac84db9a04e6b47
CHETNA GUSAIN <gusain07chetna@gmail.com>
2024-10-08 22:36:21 +0530

    completed

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import path from "path"
+// import authRoutes from "./routes/auth.routes.js"
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./SocketIO/server.js";
@@ -30,8 +31,19 @@ try {
 }
 
 //routes
+// app.use("/api/auth", authRoutes)
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
+
+// ---------------- Code for deployment -----------------
+if(process.env.NODE_ENV === "production"){
+  const dirPath = path.resolve()
+
+  app.use(express.static("./Frontend/dist"))
+  app.get("*", (req, res)=>{
+    res.sendFile(path.resolve(dirPath, "./Frontend/dist", "index.html"))
+  })
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
